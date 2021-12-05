b5425af9ea5ff576d8a93c120800f126d00f9794
arun <arunjeevaraj003@gmail.com>
2021-12-05 12:24:54 +0100

    added mm read and write to test bench, register file added to the onfi top design, register read and write tested in tb.

--- onfi/build/target.tcl
+++ onfi/build/target.tcl
@@ -3,5 +3,6 @@ set TOP                            onfi_top
 set FPGA_PART                      xc7z020clg400-1
 set_param general.maxThreads       4
 set_param general.maxBackupLogs    0
-set RTL { /home/arun/Documents/xilinx/Fpga_repo/onfi/src/onfi_top.v }
+set RTL { /home/arun/Documents/xilinx/Fpga_repo/onfi/src/onfi_top.v /home/arun/Documents/xilinx/Fpga_repo/onfi/src/onfi_reg.v }
+set SIM_RTL { /home/arun/Documents/xilinx/Fpga_repo/onfi/sim/onfi_tb.v }
 set CONSTRAINTS { /home/arun/Documents/xilinx/Fpga_repo/onfi/src/onfi_top.xdc }
--- scripts/simulate.tcl
+++ scripts/simulate.tcl
@@ -5,5 +5,5 @@ source ../build/target.tcl
 #  exec xsim ${RTL}
 #}
 
-add_wave {/onfi_tb}
-run -all
+add_wave {/onfi_tb /onfi_tb/o1/onfi_reg_control}
+run 100ns
